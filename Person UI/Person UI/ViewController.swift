e93a4c3ee7a729bf00a456691b8cc48c814cc427
armajalaruehill <armaja.lacrelle@gmail.com>
2019-09-13 13:07:08 -0500

    Person UI Challenge

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Armaja LaRue-Hill on 9/12/19.
+//  Copyright © 2019 Armaja LaRue-Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
